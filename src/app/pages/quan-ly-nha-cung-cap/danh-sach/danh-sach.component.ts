41e603e6dbb59ee5db178a63f8272a83d0d9117d
dinh-phi12 <dinhphi140399@gmail.com>
2021-03-17 20:24:21 +0700

    quản lý nhà cung cấp

--- src/app/pages/quan-ly-nha-cung-cap/danh-sach/danh-sach.component.ts
+++ src/app/pages/quan-ly-nha-cung-cap/danh-sach/danh-sach.component.ts
@@ -1,113 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NbToastrService } from '@nebular/theme';
-import { CRUDBaseService } from 'app/shared/services/crud-base.service';
-import { environment } from 'environments/environment.prod';
-import { LocalDataSource } from 'ng2-smart-table';
+import { LocalDataSource, ViewCell } from 'ng2-smart-table';
+import { UsersService } from 'app/shared/services/user/user.service';
+import { RequestQueryBuilder } from 'nest-crud-typeorm-client';
+import { UserEntity } from 'app/shared/services/user/user.interface';
+import { DatatableAction, DatatableComponent, DatatableService } from 'ngn-datatable';
+import { RoleEnum } from 'app/shared/services/role/role.interface';
+import { NhaCungCapEntity } from 'app/shared/services/nha-cung-cap/nha-cung-cap.interface';
+import { NhaCungCapService } from 'app/shared/services/nha-cung-cap/nha-cung-cap.service';
 
 @Component({
-  selector: 'ngx-danh-sach',
+  selector: 'ngx-quan-ly-tai-khoan',
   templateUrl: './danh-sach.component.html',
   styleUrls: ['./danh-sach.component.scss'],
 })
-export class DanhSachComponent implements OnInit {
+export class DanhSachComponent {
+  @ViewChild('table', { static: false })
+  table: DatatableComponent<NhaCungCapEntity>;
 
-  settings = {
-    pager: {
-      display: true,
-      perPage: 7,
-    },
-    add: {
-      addButtonContent: '<i class="nb-plus"></i>',
-      createButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-      confirmCreate: true,
-    },
-    edit: {
-      editButtonContent: '<i class="nb-edit"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-      confirmSave: true,
-    },
-    delete: {
-      deleteButtonContent: '<i class="nb-trash"></i>',
-      confirmDelete: true,
-    },
-    columns: {
-      id: {
-        title: 'ID',
-        type: 'number',
-      },
-      name: {
-        title: 'Tên nhà người dùng',
-        type: 'string',
-      },
-    },
+
+  datatableService: DatatableService<NhaCungCapEntity> = {
+    service: this.nhaCungCapService,
+    primaryField: 'nhaCungCapId',
+    builder: this.getBuilder.bind(this),
   };
-
-  source: LocalDataSource = new LocalDataSource();
-  loadDataTable() {
-    this.crudBaseService.get(`${environment.rest}/nha-cung-cap`).subscribe(
-      (value: { allNhaCungCap: [] }) => {
-        this.source.load(value.allNhaCungCap);
-      },
-      (err) => {
-        this.toast.danger(err.error.message);
-      },
-    );
-  }
+  actions: DatatableAction<NhaCungCapEntity>[] = [
+    { name: 'quick-edit' },
+    { name: 'delete' },
+  ];
   constructor(
-    private crudBaseService: CRUDBaseService,
     private toast: NbToastrService,
+    private nhaCungCapService: NhaCungCapService,
   ) {
-    this.loadDataTable();
   }
-
-  onDeleteConfirm(event): void {
-    if (window.confirm('Are you sure you want to delete?')) {
-      this.crudBaseService
-        .delete(`${environment.rest}/nha-cung-cap/${event.data.id}`)
-        .subscribe((values: { message: string }) => {
-          if (values) {
-            this.toast.success(values.message);
-          }
-        });
-      event.confirm.resolve();
-    } else {
-      event.confirm.reject();
-    }
+  loadDataTable() {
+    this.table.loadData();
   }
-  onCreateConfirm(event) {
-    this.crudBaseService
-      .post(`${environment.rest}/nha-cung-cap`, event.newData)
-      .subscribe(
-        (value: { message: string }) => {
-          this.toast.success(value.message);
-        },
-        (err) => {
-          this.toast.danger(err.message);
-        },
-        () => {
-          this.loadDataTable();
-        },
-      );
+  getBuilder(builder: RequestQueryBuilder) {
+    // tslint:disable-next-line:max-line-length
+    builder.select(['tenNhaCungCap', 'url','phoneNumber', 'address']);   
   }
-  onSaveConfirm(event) {
-    this.crudBaseService
-      .put(`${environment.rest}/nha-cung-cap/${event.data.id}`, event.newData)
-      .subscribe(
-        (value: { message: string }) => {
-          this.toast.success(value.message);
-        },
-        (err) => {
-          this.toast.danger(err.message);
-        },
-        () => {
-          this.loadDataTable();
-        },
-      );
-  }
-
-  ngOnInit(): void {
-  }
-
 }
