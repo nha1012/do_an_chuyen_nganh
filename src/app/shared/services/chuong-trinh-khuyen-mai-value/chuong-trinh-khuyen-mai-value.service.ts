73d83fff747d38a7c90ec7578c3c7b1961d6ac59
Nha Nguyen <nguyenphucnha111@gmail.com>
2021-03-31 08:44:21 +0700

    thêm service

---
+++ src/app/shared/services/chuong-trinh-khuyen-mai-value/chuong-trinh-khuyen-mai-value.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment.prod';
+import { CrudBaseService } from 'nest-crud-typeorm-client';
+import { ChuongTrinhKhuyenMaiValueEntity } from './chuong-trinh-khuyen-mai-value.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChuongTrinhKhuyenMaiValueService extends CrudBaseService<ChuongTrinhKhuyenMaiValueEntity> {
+  constructor(httpClient: HttpClient) {
+    super(
+      {
+        apiUrl: environment.rest,
+        entity: 'chuong-trinh-khuyen-mai',
+      },
+      httpClient,
+    );
+  }
+}
