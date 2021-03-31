0645249753af914b2d63f483ddc59009c351e145
Nha Nguyen <nguyenphucnha111@gmail.com>
2021-03-31 10:04:23 +0700

    quản lý chương trình khuyến mãi

--- src/app/pages/quan-ly-khuyen-mai/danh-sach/danh-sach.component.ts
+++ src/app/pages/quan-ly-khuyen-mai/danh-sach/danh-sach.component.ts
@@ -3,6 +3,8 @@ import { ChuongTrinhKhuyenMaiService } from './../../../shared/services/chuong-t
 import { Component, ViewChild } from '@angular/core';
 import { NbToastrService } from '@nebular/theme';
 import { DatatableAction, DatatableComponent, DatatableService } from 'ngn-datatable';
+import { CRUD_MESSAGES } from 'app/shared/messages/crud.messages';
+import { RequestQueryBuilder } from 'nest-crud-client';
 
 @Component({
   selector: 'ngx-danh-sach',
@@ -12,13 +14,15 @@ import { DatatableAction, DatatableComponent, DatatableService } from 'ngn-datat
 export class DanhSachComponent {
   @ViewChild('table', { static: false })
   table: DatatableComponent<ChuongTrinhKhuyenMaiEntity>;
-
+  chuongTrinhKhuyenMai: ChuongTrinhKhuyenMaiEntity = {};
   filterEntity: ChuongTrinhKhuyenMaiEntity = {
     chuongTrinhKhuyenMaiId: '',
   };
+  tgKhuyenMai: any;
   datatableService: DatatableService<ChuongTrinhKhuyenMaiEntity> = {
     service: this.chuongTrinhKhuyenMaiService,
     primaryField: 'chuongTrinhKhuyenMaiId',
+    builder: this.getBuilder.bind(this),
   };
   actions: DatatableAction<ChuongTrinhKhuyenMaiEntity>[] = [
     { name: 'quick-edit' },
@@ -29,7 +33,47 @@ export class DanhSachComponent {
     private chuongTrinhKhuyenMaiService: ChuongTrinhKhuyenMaiService,
   ) {
   }
+  getBuilder(builder: RequestQueryBuilder) {
+    if (this.tgKhuyenMai) {
+      this.tgKhuyenMai &&
+        (this.tgKhuyenMai as any).start &&
+        builder.setFilter({
+          field: 'startDate',
+          operator: '$gte',
+          value: ((this.tgKhuyenMai as any).start as Date).toJSON(),
+        });
+      this.tgKhuyenMai &&
+        (this.tgKhuyenMai as any).end &&
+        builder.setFilter({
+          field: 'endDate',
+          operator: '$lte',
+          value: ((this.tgKhuyenMai as any).end as Date).toJSON(),
+        });
+    }
+  }
   loadDataTable() {
     this.table.loadData();
+  }
+  isValid() {
+    if (!this.chuongTrinhKhuyenMai.startDate) {
+      throw new Error('Vui lòng chọn ngày bắt đầu');
+    }
+    if (!this.chuongTrinhKhuyenMai.endDate) {
+      throw new Error('Vui lòng chọn ngày kết thúc');
+    }
+    if (!this.chuongTrinhKhuyenMai.tenChuongTrinh) {
+      throw new Error('Vui lòng nhập tên chương trình');
+    }
+    return true;
+  }
+  async themCTKM() {
+    try {
+      this.isValid();
+      await this.chuongTrinhKhuyenMaiService.create(this.chuongTrinhKhuyenMai).toPromise();
+      this.toast.success(CRUD_MESSAGES.SUCCESS_ADD);
+      this.loadDataTable();
+    } catch (error) {
+      this.toast.warning(error);
+    }
   }
 }
